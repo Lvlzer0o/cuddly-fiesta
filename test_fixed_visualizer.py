5f08dbc65abaaeeee0e2e94f056f2e57ec413cb2
lvlzer0o <trentoncadena97@gmail.com>
2025-06-20 03:03:09 -0700

    Add fixed ECG visualizer with dark theme and animation support

---
+++ test_fixed_visualizer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Test the fixed ECG visualizer
+"""
+
+def main():
+    """Launch the fixed ECG visualizer."""
+    from cuddly_fiesta.ecg_visualizer_fixed import run_visualizer_fixed
+    run_visualizer_fixed()
+
+if __name__ == "__main__":
+    main()
